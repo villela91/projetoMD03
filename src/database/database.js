ffb74bcac25a5853c4ae6ebd0e46dbb4c5e7cca4
LUIZ VILLELA <106358282+villela91@users.noreply.github.com>
2022-08-11 21:11:59 -0300

    https://github.com/villela91/projetoMD03

---
+++ src/database/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');//importação do mongoose no meu arquivo
+//função de conexão ao meu banco MFseries-db lá no mongo compass.
+function connectTodatabase(){
+    mongoose
+    .connect('mongodb://localhost27017/MFseries',{
+        useNewUrlparser: true,//para não ter problema com a url..
+        useUnifiedTopology:true, //unificando a topologia de rede para não dar ruim na conexa
+
+
+    })
+    .then(() => console.log('MongoDB Conectado ;)!!!'))
+    .catch((erro) =>
+    console.log(` erro ao conctar com o  MongoDB!, erro:`)
+    );
+}
+
+module.exports = connectTodatabase;
--- src/mocks/series.js
+++ src/mocks/series.js
@@ -1,27 +1,70 @@
 const MFseries = [
-    {
-      id: "1",
-      nome: "HIMYM",
-      ano: "2005",
-      temporadas:9,
-      genero: "sitcom",
-      elenco:["",""]
-    },
-    {
-      id: "2",
-      nome: "The Office",
-      ano: "2005",
-      temporadas:9,
-      genero: "sitcom",
-      elenco:["",""]
-    },
-    {
-      id: "3",
-      nome: "Dexter",
-      ano: "2006",
-      temporadas:8,
-      genero: "Mistério",
-      elenco:["",""]
-    },
-  ];
-  module.exports = MFseries;
+  {
+    id: "1",
+    nome: "HIMYM",
+    ano: "2005",
+    temporadas: 9,
+    genero: "sitcom",
+    elenco: [
+      "Josh Radnor",
+      "Neil Patrick Harris",
+      "Alyson Hannigan",
+      "Cobie Smulders",
+      "Jason Segel",
+      "Cristin Milioti",
+    ],
+  },
+  {
+    id: "2",
+    nome: "The Office",
+    ano: "2005",
+    temporadas: 9,
+    genero: "sitcom",
+    elenco: [
+      "Steve Carell",
+      "Jenna Fischer",
+      "John Krasinski",
+      "Rainn Wilson",
+      "Mindy Kaling",
+      "Angela Kinsey",
+      "B. J. Novak",
+      "Melora Hardin",
+      "Creed Bratton",
+      "Phyllis Smith",
+      "Brian Baumgartne",
+      "Oscar Nunez",
+    ],
+  },
+  {
+    id: "3",
+    nome: "Dexter",
+    ano: "2006",
+    temporadas: 8,
+    genero: "Mistério",
+    elenco: [
+      "Michael C. Hall",
+      "Jennifer Carpenter",
+      "David Zayas",
+      "Erik King",
+      "C.S. Lee",
+      "Lauren Vélez",
+    ],
+  },
+  {
+    id: "4",
+    nome: "The Sandman",
+    ano: "2022 ",
+    temporadas: 1,
+    genero: "Drama ",
+    elenco: [
+      "Tom Sturridge",
+      "Jenna Coleman",
+      "Gwendoline Christie",
+      "Kirby Howell-Baptiste",
+      "Boyd Holbrook",
+      "Lauren Vélez","Vivienne Acheampong","Stephen Fry","Asim Chaudhry","Sanjeev Bhaskar","Donna Preston",
+
+    ],
+  },
+];
+module.exports = MFseries;
