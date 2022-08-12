3a08ca71845bcf79d2fff63fa53c8817b7d75db1
LUIZ VILLELA <106358282+villela91@users.noreply.github.com>
2022-08-11 22:06:31 -0300

    https://github.com/villela91/projetoMD03.git

--- src/database/database.js
+++ src/database/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');//importação do mongoose no meu arquivo
 //função de conexão ao meu banco MFseries-db lá no mongo compass.
 function connectTodatabase(){
     mongoose
-    .connect('mongodb://localhost27017/MFseries',{
+    .connect('mongodb://localhost:27017/MFseries',{
         useNewUrlparser: true,//para não ter problema com a url..
         useUnifiedTopology:true, //unificando a topologia de rede para não dar ruim na conexa
 
@@ -10,7 +10,7 @@ function connectTodatabase(){
     })
     .then(() => console.log('MongoDB Conectado ;)!!!'))
     .catch((erro) =>
-    console.log(` erro ao conctar com o  MongoDB!, erro:`)
+    console.log(` erro ao conctar com o  MongoDB!, erro: ${erro}`)
     );
 }
 
